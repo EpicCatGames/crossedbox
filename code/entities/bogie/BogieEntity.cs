98ef11e8f639abd9f6058f0d17ad19dbeb4733f9
SupinePandora43 <supinepandoraggg@gmail.com>
2021-08-16 15:53:41 +0500

    BogieEntity WIP

---
+++ code/entities/bogie/BogieEntity.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox;
+
+[Library( "ent_bogie", Title = "Bogie", Spawnable = true )]
+public partial class BogieEntity : Prop
+{
+	private Prop frontWheel;
+	private Prop rearWheel;
+
+	public BogieEntity()
+	{
+
+	}
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/train/1520/bogies/simple.vmdl" );
+	}
+
+}
