6d56a0082341f8865e2e63a057a3d899ca831757
SupinePandora43 <supinepandoraggg@gmail.com>
2021-08-24 14:11:35 +0500

    implement weld using joint

---
+++ code/tools/JointWeld.cs
@@ -0,0 +1,55 @@
+﻿namespace Sandbox.Tools
+{
+	[Library( "tool_jointweld", Title = "Joint Weld", Description = "Fixed weld" )]
+	public partial class JointWeldTool : BaseTool
+	{
+		PhysicsBody physicsBody;
+
+		public override void Simulate()
+		{
+			if ( !Host.IsServer )
+				return;
+
+			using ( Prediction.Off() )
+			{
+				if ( !Input.Pressed( InputButton.Attack1 ) )
+					return;
+
+				var startPos = Owner.EyePos;
+				var dir = Owner.EyeRot.Forward;
+
+				var tr = Trace.Ray( startPos, startPos + dir * MaxTraceDistance )
+					.Ignore( Owner )
+					.Run();
+
+				if ( !tr.Hit )
+					return;
+
+				if ( !tr.Entity.IsValid() )
+					return;
+
+				if ( !(tr.Body.IsValid() && (tr.Body.PhysicsGroup != null) && tr.Body.Entity.IsValid()) ) return;
+
+				if ( !physicsBody.IsValid() )
+				{
+					physicsBody = tr.Body;
+				}
+				else
+				{
+					PhysicsJoint.Weld
+						.From( physicsBody )
+						.To( tr.Body )
+						.WithPivot( tr.EndPos )
+						.WithBasis( Rotation.LookAt( tr.Normal ) * Rotation.From( new Angles( 90, 0, 0 ) ) )
+						.Create();
+
+					physicsBody.PhysicsGroup?.Wake();
+					tr.Body.PhysicsGroup?.Wake();
+
+					physicsBody = null;
+				}
+				CreateHitEffects( tr.EndPos );
+			}
+		}
+	}
+}
